989f4f120a377376fc9180acea8bd7d677b9bbcb
mariaalouisaa <maria.louisa.castro@gmail.com>
2021-06-17 21:47:17 +0100

    Added javascript and css to form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-alert("hello");
+function askUser(event) {
+  let answer = document.querySelector("input").value;
+  if (answer < 5) {
+    alert("Damn, that's harsh.");
+  } else {
+    if (answer <= 10) {
+      alert("We've found a fan!");
+    } else {
+      alert("Are you sure you typed that right?");
+    }
+  }
+}
+
+let form = document.querySelector("#question");
+form.addEventListener("submit", askUser);
